0abf4b4529b1f64410a708d26e527975426ec5d8
Andy Garcia <acgarzeah@gmail.com>
2021-09-17 00:03:24 -0700

    Added postgres support for conversation and message model

--- server/src/models/conversation.model.js
+++ server/src/models/conversation.model.js
@@ -1,18 +1,49 @@
-const jwt = require("jsonwebtoken");
+const { pool } = require("../services/db");
+const toCamelCase = require("../utils/toCamelCase.utils");
+const Message = require("./message.model");
 
-// Protecting routes w/ JWT
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  // If authHeader exists then we split
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) return res.sendStatus(401);
+const Conversation = {
+  // Retrieves all of a user's conversations
+  find: async (host) => {
+    // Want to join the participants and conversations together
+    const { rows } = await pool.query(
+      `SELECT *
+        FROM conversations
+        WHERE host_id = $1;`,
+      [host]
+    );
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    return toCamelCase(rows);
+  },
 
-    req.user = user;
-    next();
-  });
+  // Retrieves one conversation between host and participant
+  findById: async (hostId, participantId) => {
+    // Want to find the conversation associated with the users
+    const conversation = await pool.query(
+      `SELECT id
+        FROM conversations
+        WHERE host_id = $1
+          AND participant_id = $2;`,
+      [hostId, participantId]
+    );
+
+    // Find all the messages associated with the conversation
+    const { rows } = Message.findById(conversation[0].id);
+
+    return toCamelCase(rows)[0];
+  },
+
+  // Creates a new conversation
+  insert: async (hostId, participantId) => {
+    const { rows } = await pool.query(
+      `INSERT INTO conversations (host_id, participant_id)
+        VALUES ($1, $2)
+        RETURNING *;`,
+      [hostId, participantId]
+    );
+
+    return toCamelCase(rows)[0];
+  }
 };
 
-module.exports = verifyToken;
+module.exports = Conversation;
--- server/src/models/message.model.js
+++ server/src/models/message.model.js
@@ -1,3 +1,33 @@
-const Message = {};
+const { pool } = require("../services/db");
+const toCamelCase = require("../utils/toCamelCase.utils");
+
+const Message = {
+  // Retrieves all messages for a given conversation
+  findById: async (conversationId) => {
+    // Want to find the messages associated with a conversation
+    const { rows } = await pool.query(
+      `SELECT *
+        FROM messages
+        WHERE conversation_id = $1
+        ORDER BY created_at DESC
+      `,
+      [conversationId]
+    );
+
+    return toCamelCase(rows)[0];
+  },
+
+  // Creates a new message
+  insert: async (conversationId, authorId, content) => {
+    const { rows } = await pool.query(
+      `INSERT INTO messages (conversation_id, author_id, content)
+        VALUES ($1, $2, $3)
+        RETURNING *;`,
+      [conversationId, authorId, content]
+    );
+
+    return toCamelCase(rows)[0];
+  }
+};
 
 module.exports = Message;
